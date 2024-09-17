e380e6b6772b8f7c1dfd60fe3c9a12847c589d7f
lieftsatyamsrivastava <satyamsrivastava384@gmail.com>
2024-09-17 23:34:59 +0530

    ts fix 2

--- base-dashboard-next-js/src/app/page.tsx
+++ base-dashboard-next-js/src/app/page.tsx
@@ -2,9 +2,6 @@ import PageSidebar from "@/components/app/page-sidebar"
 import PageHeader from "@/components/app/page-header"
 import PageMain from "@/components/app/page-main"
 
-export const description =
-  "A products dashboard with a sidebar navigation and a main content area. The dashboard has a header with a search input and a user menu. The sidebar has a logo, navigation links, and a card with a call to action. The main content area shows an empty state with a call to action."
-
 export default function Dashboard() {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
